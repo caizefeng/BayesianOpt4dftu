d14dbb1d9f8cf9a4282b8bbc1d5733a3259fc595
Zavier Cai <caizefeng18@gmail.com>
2023-09-25 11:44:47 -0400

    Use singleton class to store configs

---
+++ BayesOpt4dftu/Config.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+
+class Config:
+    _instance = None
+
+    def __new__(cls, config_file="input.json"):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            cls._instance._load_config(config_file)
+        return cls._instance
+
+    def _load_config(self, config_file):
+        with open(config_file, "r") as f:
+            data = json.load(f)
+
+        vasp_env_params = data['vasp_env']
+        # Command to run VASP executable.
+        self.vasp_run_command = vasp_env_params.get('vasp_run_command', 'srun -n 54 vasp_ncl')
+        # Define the name for output file.
+        self.out_file_name = vasp_env_params.get('out_file_name', 'vasp.out')
+        # Define the path direct to the VASP pseudopotential.
+        self.vasp_pp_path = vasp_env_params.get('vasp_pp_path', '/home/maituoy/pp_vasp/')
+        self.dry_run = vasp_env_params.get('dry_run', False)
+
+        os.environ['VASP_PP_PATH'] = self.vasp_pp_path
+
+        bo_params = data['bo']
+        self.k = float(bo_params.get('kappa', 5))
+        self.a1 = bo_params.get('alpha1', 0.25)
+        self.a2 = bo_params.get('alpha2', 0.75)
+        self.which_u = tuple(bo_params.get('which_u', [1, 1]))
+        self.urange = tuple(bo_params.get('urange', [-10, 10]))
+        self.br = tuple(bo_params.get('br', [5, 5]))
+        self.import_kpath = bo_params.get('import_kpath', False)
+        self.elements = bo_params.get('elements', ['In', 'As'])
+        self.iteration = bo_params.get('iteration', 50)
+        self.threshold = bo_params.get('threshold', 0.0001)
--- BayesOpt4dftu/main.py
+++ BayesOpt4dftu/main.py
@@ -1,5 +1,6 @@
 from BayesOpt4dftu.core import *
 from BayesOpt4dftu.BoLogging import BoLogging
+from BayesOpt4dftu.Config import Config
 
 
 def main():
@@ -10,41 +11,20 @@ def main():
 
     driver_logger.info("Task begins.")
     driver_logger.info("Loading configuration from file input.json ...")
-    with open("input.json", "r") as f:
-        data = json.load(f)
 
-    vasp_env_params = data['vasp_env']
-    # Command to run VASP executable.
-    vasp_run_command = vasp_env_params.get('vasp_run_command', 'srun -n 54 vasp_ncl')
-    # Define the name for output file.
-    out_file_name = vasp_env_params.get('out_file_name', 'vasp.out')
-    # Define the path direct to the VASP pseudopotential.
-    vasp_pp_path = vasp_env_params.get('vasp_pp_path', '/home/maituoy/pp_vasp/')
-    dry_run = vasp_env_params.get('dry_run', False)
-
-    bo_params = data['bo']
-    k = float(bo_params.get('kappa', 5))
-    a1 = bo_params.get('alpha1', 0.25)
-    a2 = bo_params.get('alpha2', 0.75)
-    which_u = tuple(bo_params.get('which_u', [1, 1]))
-    urange = tuple(bo_params.get('urange', [-10, 10]))
-    br = tuple(bo_params.get('br', [5, 5]))
-    import_kpath = bo_params.get('import_kpath', False)
-    elements = bo_params.get('elements', ['In', 'As'])
-    iteration = bo_params.get('iteration', 50)
-    threshold = bo_params.get('threshold', 0.0001)
-
-    os.environ['VASP_PP_PATH'] = vasp_pp_path
+    # Initialize and read all configurations
+    config = Config("input.json")
 
     driver_logger.info("Configuration loaded.")
-
     driver_logger.info("DFT calculations begin.")
 
-    if dry_run:
+    if config.dry_run:
         dft_logger.info("Dry run set to True.")
-        calculate(command=vasp_run_command, outfilename=out_file_name, method='hse', import_kpath=import_kpath,
+        calculate(command=config.vasp_run_command, outfilename=config.out_file_name, method='hse',
+                  import_kpath=config.import_kpath,
                   is_dry=True)
-        calculate(command=vasp_run_command, outfilename=out_file_name, method='dftu', import_kpath=import_kpath,
+        calculate(command=config.vasp_run_command, outfilename=config.out_file_name, method='dftu',
+                  import_kpath=config.import_kpath,
                   is_dry=True)
         dft_logger.info("No actual calculations were performed. Review the input files before proceeding.")
         driver_logger.info("Dry run executed.")
@@ -52,7 +32,7 @@ def main():
         dft_logger.info("Dry run set to False.")
 
         header = []
-        for i, u in enumerate(which_u):
+        for i, u in enumerate(config.which_u):
             header.append('U_ele_%s' % str(i + 1))
 
         if os.path.exists('./u_tmp.txt'):
@@ -64,7 +44,8 @@ def main():
         dft_logger.info("Production run initiated.")
         dft_logger.info("Hybrid DFT calculation begins.")
 
-        calculate(command=vasp_run_command, outfilename=out_file_name, method='hse', import_kpath=import_kpath,
+        calculate(command=config.vasp_run_command, outfilename=config.out_file_name, method='hse',
+                  import_kpath=config.import_kpath,
                   is_dry=False)
 
         dft_logger.info("Hybrid DFT calculation finished.")
@@ -73,16 +54,18 @@ def main():
         dft_logger.info("GGA+U calculations begin.")
 
         obj = 0
-        for i in range(iteration):
-            calculate(command=vasp_run_command, outfilename=out_file_name, method='dftu', import_kpath=import_kpath,
+        for i in range(config.iteration):
+            calculate(command=config.vasp_run_command, outfilename=config.out_file_name, method='dftu',
+                      import_kpath=config.import_kpath,
                       is_dry=False)
-            db = delta_band(bandrange=br, path='./')
+            db = delta_band(bandrange=config.br, path='./')
             db.deltaBand()
 
-            bayesian_opt = bayesOpt_DFTU(path='./', opt_u_index=which_u, u_range=urange, kappa=k, a1=a1, a2=a2,
-                                         elements=elements)
+            bayesian_opt = bayesOpt_DFTU(path='./', opt_u_index=config.which_u, u_range=config.urange, kappa=config.k,
+                                         a1=config.a1, a2=config.a2,
+                                         elements=config.elements)
             obj_next = bayesian_opt.bo()
-            if abs(obj_next - obj) <= threshold:
+            if abs(obj_next - obj) <= config.threshold:
                 bo_logger.info("Convergence reached, exiting.")
                 break
             obj = obj_next
@@ -97,7 +80,7 @@ def main():
 
         driver_logger.info("Bayesian Optimization finished.")
 
-        os.system('mv ./u_tmp.txt ./u_kappa_%s_a1_%s_a2_%s.txt' % (k, a1, a2))
+        os.system('mv ./u_tmp.txt ./u_kappa_%s_a1_%s_a2_%s.txt' % (config.k, config.a1, config.a2))
 
     driver_logger.info("Task completed.")
 
