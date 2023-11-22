0adfd4d0787abd9ad7f49d96c93ea45599cd5e45
Zavier Cai <caizefeng18@gmail.com>
2023-11-22 15:14:10 -0500

    Add a main process timer

--- BayesOpt4dftu/main.py
+++ BayesOpt4dftu/main.py
@@ -4,7 +4,7 @@ from BayesOpt4dftu.bo import *
 from BayesOpt4dftu.configuration import Config
 from BayesOpt4dftu.delta_all import DeltaAll
 from BayesOpt4dftu.dft import VaspInit, DftManager
-from BayesOpt4dftu.io_utils import TempFileManager
+from BayesOpt4dftu.io_utils import TempFileManager, task_timer
 from BayesOpt4dftu.logging import BoLoggerGenerator
 from . import __version__
 
@@ -15,65 +15,63 @@ def main():
     args = parser.parse_args()  # This will print version and exit if --version is provided
 
     driver_logger = BoLoggerGenerator.get_logger("Driver")
-    driver_logger.info("Task begins.")
+    with task_timer("Task", driver_logger):
 
-    # Initialize and read all configurations
-    config = Config("input.json")
-    TempFileManager.init_config(config)
-    DftManager.init_config(config)
-    VaspInit.init_config(config)
-    DeltaAll.init_config(config)
-    BoDftuIterator.init_config(config)
+        # Initialize and read all configurations
+        config = Config("input.json")
+        TempFileManager.init_config(config)
+        DftManager.init_config(config)
+        VaspInit.init_config(config)
+        DeltaAll.init_config(config)
+        BoDftuIterator.init_config(config)
 
-    dft_manager = DftManager()
-    if config.dry_run:
-        driver_logger.info("Dry run set to True.")
-        if not config.dftu_only:
-            dft_manager.run_task(method='hse')
-        dft_manager.run_task(method='dftu')
-    else:
-        driver_logger.info("Dry run set to False.")
-
-        temp_file_manager = TempFileManager()
-        temp_file_manager.setup_temp_files()
-
-        if not config.dftu_only:
-            dft_manager.run_task(method='hse')
-
-        bo_iterator = BoDftuIterator()
-        delta = DeltaAll()
-        for i in range(config.iteration):
+        dft_manager = DftManager()
+        if config.dry_run:
+            driver_logger.info("Dry run set to True.")
+            if not config.dftu_only:
+                dft_manager.run_task(method='hse')
             dft_manager.run_task(method='dftu')
-            delta.compute_delta()
-            delta.write_delta()
+        else:
+            driver_logger.info("Dry run set to False.")
 
-            # Print baseline band gap for reference
-            if i == 0:
-                if config.baseline == 'hse':
-                    driver_logger.info(f"Band gap from hybrid DFT calculation: {delta.dg.get_baseline_gap()} eV")
-                elif config.baseline == 'gw':
-                    driver_logger.info(f"Band gap from GW calculation: {delta.dg.get_baseline_gap()} eV")
+            temp_file_manager = TempFileManager()
+            temp_file_manager.setup_temp_files()
 
-            bo_iterator.next()
-            if bo_iterator.converge():
-                break
+            if not config.dftu_only:
+                dft_manager.run_task(method='hse')
 
-        # Visualize the optimization process and retrieve optimized U
-        bo_iterator.plot()
-        optimal_u, _ = bo_iterator.get_optimal()
+            bo_iterator = BoDftuIterator()
+            delta = DeltaAll()
+            for i in range(config.iteration):
+                dft_manager.run_task(method='dftu')
+                delta.compute_delta()
+                delta.write_delta()
 
-        if config.get_optimal_band:
-            bo_iterator.update_u_config(optimal_u)
-            dft_manager.run_task(method='dftu')
-            delta.compute_delta()
-            delta.write_delta()
-            driver_logger.info("An additional DFT+U calculation using optimal U values performed.")
+                # Print baseline band gap for reference
+                if i == 0:
+                    if config.baseline == 'hse':
+                        driver_logger.info(f"Band gap from hybrid DFT calculation: {delta.dg.get_baseline_gap()} eV")
+                    elif config.baseline == 'gw':
+                        driver_logger.info(f"Band gap from GW calculation: {delta.dg.get_baseline_gap()} eV")
 
-        dft_manager.finalize()
-        bo_iterator.finalize()
-        temp_file_manager.clean_up_temp_files()
+                bo_iterator.next()
+                if bo_iterator.converge():
+                    break
 
-    driver_logger.info("Task completed.")
+            # Visualize the optimization process and retrieve optimized U
+            bo_iterator.plot()
+            optimal_u, _ = bo_iterator.get_optimal()
+
+            if config.get_optimal_band:
+                bo_iterator.update_u_config(optimal_u)
+                dft_manager.run_task(method='dftu')
+                delta.compute_delta()
+                delta.write_delta()
+                driver_logger.info("An additional DFT+U calculation using optimal U values performed.")
+
+            dft_manager.finalize()
+            bo_iterator.finalize()
+            temp_file_manager.clean_up()
 
 
 if __name__ == "__main__":
